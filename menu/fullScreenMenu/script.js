dd36e2316e2bed29b95314e4845d140b89637f7d
yaroprof <crosskyiv@gmail.com>
2023-05-29 17:47:16 +0300

    Fullscreen menu

---
+++ menu/fullScreenMenu/script.js
@@ -0,0 +1,21 @@
+function openFullscreenMenu() {
+  var fullscreenMenu = document.getElementById("fullscreen-menu");
+  fullscreenMenu.classList.add("show-menu");
+}
+
+function closeFullscreenMenu() {
+  var fullscreenMenu = document.getElementById("fullscreen-menu");
+  fullscreenMenu.classList.remove("show-menu");
+}
+
+
+
+
+// var fullScreenMenu = document.getElementById('fullscreen-menu');
+
+// function openFullScreenMenu() {
+//   fullScreenMenu.classList.add('show-menu');
+// }
+// function closeFullScreenMenu() {
+//   fullScreenMenu.classList.remove('show-menu');
+// }
