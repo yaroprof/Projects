3ce736ce74b82bbd80cf3cbfa5f9621ace0b8463
yaroprof <crosskyiv@gmail.com>
2023-06-01 17:51:35 +0300

    final commit

--- menu/react-navbar-app/src/components/Navbar.js
+++ menu/react-navbar-app/src/components/Navbar.js
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiMenu, FiX } from 'react-icons/fi';
+import './Navbar.css';
 
 function Navbar() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <Link to="/" className='logo' onClick={()=>setOpen(false)}>Logo</Link>
+      <Link to="/" className="nav-logo" onClick={() => setOpen(false)}>
+        Logo
+      </Link>
 
-      <ul className="">
+      <ul className={open ? 'nav-links active' : 'nav-links'}>
         <li className="nav-item">
-          <Link to="/" className='nav-link' onClick={() => setOpen(false)}>Home</Link>
+          <Link to="/" className="nav-link" onClick={() => setOpen(false)}>
+            Home
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/about" className='nav-link' onClick={() => setOpen(false)}>About</Link>
+          <Link to="/about" className="nav-link" onClick={() => setOpen(false)}>
+            About
+          </Link>
         </li>
         <li className="nav-item">
-          <Link to="/contact" className='nav-link' onClick={() => setOpen(false)}>Contact</Link>
+          <Link to="/contact" className="nav-link" onClick={() => setOpen(false)}>
+            Contact
+          </Link>
         </li>
       </ul>
 
-      <div>{ open ? <FiX /> : <FiMenu />}</div>
+      <div onClick={() => setOpen(!open)} className="nav-icon">
+        {open ? <FiX /> : <FiMenu />}
+      </div>
     </nav>
   );
 }
