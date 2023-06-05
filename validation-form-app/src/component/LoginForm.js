25ffa3adcf15c0efc5603ed2c4dd02aea5747601
yaroprof <crosskyiv@gmail.com>
2023-06-05 16:19:13 +0300

    final loginForm commit

--- validation-form-app/src/component/LoginForm.js
+++ validation-form-app/src/component/LoginForm.js
@@ -2,20 +2,72 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = {};
+
+    if (email.trim() === '') {
+      errors.email = 'Required field';
+    } else if (!isValidEmail(email)) {
+      errors.email = 'Email format is incorrect';
+    }
+
+    if (Object.keys(errors).length === 0) {
+      alert('Success');
+      setEmail('');
+      setPassword('');
+    } else {
+      setErrors(errors);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    } else if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
-    <form className="login-form">
+    <form onSubmit={handleSubmit} className="login-form">
       <div className="form-group">
         <label htmlFor="email"> E-mail:</label>
         <input
           type="text"
           id="email"
           name="email"
-          // value={email}
-          // onChange={handleChange}
-          // className={errors.email ? 'input-error' : ''}
-        
+          value={email}
+          onChange={handleChange}
+          className={errors.email ? 'input-error' : ''}
         />
+        {errors.email && <div className="error">{errors.email}</div>}
       </div>
+      <div className="form-group">
+        <label htmlFor="password">PW:</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          className={errors.password ? 'input-error' : ''}
+        />
+        {errors.password && <div className="error">{errors.password}</div>}
+      </div>
+
+      <button type="submit" className="submit-btn">
+        Login
+      </button>
     </form>
   );
 };
